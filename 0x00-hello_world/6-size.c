2443bdf75180a0558eb73868ea00167cde35d895
yunusfaisel <yunusfaisel0@gmail.com>
2023-08-12 14:45:54 -0700

    test 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,13 +4,13 @@
  * Return: 0 (Success)
  */
 
-int main(void) 
+int main(void)
 {
-        char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
+char a;
+int b;
+long int c;
+long long int d;
+float f;
 
 printf("Size of a char: %d byte(s)\n", sizeof(a));
 printf("Size of an int: %d byte(s)\n", sizeof(b));
