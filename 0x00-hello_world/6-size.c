66a107fe4bd869d5afe0c46643ccb6bf457ec64d
yunusfaisel <yunusfaisel0@gmail.com>
2023-08-12 14:07:10 -0700

    test 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,18 +4,17 @@
  * Return: 0 (Success)
  */
 
-int main(void)
-{
+int main(void) {
 	char a;
 	int b;
 	long int c;
 	long long int d;
 	float f;
 
-printf("Size of a char: %d  byte(s)\n", sizeof(a));
-printf("Size of a int: %d  byte(s)\n", sizeof(b));
-printf("Size of a long int: %d  byte(s)\n", sizeof(c));
-printf("Size of a long long int: %d  byte(s)\n", sizeof(d));
-printf("Size of a float: %d  byte(s)\n", sizeof(f));
-return (0);
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+        printf("Size of a int: %d byte(s)\n", sizeof(b));
+        printf("Size of a long int: %d byte(s)\n", sizeof(c));
+        printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+        printf("Size of a float: %d byte(s)\n", sizeof(f));
+        return (0);
 }
